5a3cd560ff105370685c3e154aec65a7157e7858
GuilhermeSousaBorge <guilhermepllay157@gmail.com>
2024-09-01 17:56:42 -0300

    feito

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,23 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, disabled }: IButtonProps) => {
+  return <button
+    onClick={onClick}
+    disabled={disabled}
+    style={{
+      width: "100%",
+      height: "42px",
+      backgroundColor: "#81259D",
+      color: "#FFF",
+      border: "1px solid #81259D",
+      borderRadius: "21px",
+      cursor: "pointer",
+      opacity: disabled ? 0.6 : 1,
+    }}
+  >
+    {title}
+  </button>;
 };
 
 export default Button;
